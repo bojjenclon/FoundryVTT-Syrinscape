7ffafaf96e90fba609eec85598be5c978c87a49d
Cornell Daly <dalycornell@gmail.com>
2020-11-16 15:43:48 -0500

    Pull out handlebars registration into separate file, minor README info

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # FoundryVTT - Syrinscape (Online Player) Integration
 
+This module provides a convenient way of triggering sounds from Syrinscape's online player from within Foundry VTT. Do note that this does not route audio through Foundry - GMs/players must still use the external Syrinscape service.
+
+## How to Use
+
+The main window is accessible via typing _"/syrin"_ in chat. I recommend macroing this command for easy access.
+
+An auth token/API key must be provided in the module's settings. Without it, this module won't really do anything.
+
+The sound URL should be everything leading up to the word "play" or "stop." If copying a URL from the "Show Remote Controls" feature in the Syrinscape online master portal, you'll need to strip part of it out. A typical URL from the online player looks like: "https://www.syrinscape.com/online/frontend-api/elements/9412/play/?auth_token=XXX". You just need this part: "https://www.syrinscape.com/online/frontend-api/elements/9412".
+
+Folders can be used to subdivide sounds into related sections. All content within the Syrinscape window is sorted alphabetically.
+
 ## Todo
 
 * Better localization
 * Warning dialog when deleting non-empty folders
 * General tree traversal optimizations
 
-## Nice to have (but probably won't do)
+## Nice to Have (but probably won't do)
 
 * Support drag and drop in the syrinscape window instead of a custom move dialog
--- src/syrinscape.ts
+++ src/syrinscape.ts
@@ -15,6 +15,7 @@ import { registerSettings } from './module/settings.js';
 import { preloadTemplates } from './module/preloadTemplates.js';
 import { SyrinscapeDialogApplication } from './module/dialog/syrinscapeDialog.js';
 import { syrinFind } from './module/util.js';
+import registerHandlebars from './module/handlebars.js';
 
 /* ------------------------------------ */
 /* Initialize module					*/
@@ -29,51 +30,7 @@ Hooks.once('init', async function () {
 	// Register custom module settings
 	registerSettings();
 
-	Handlebars.registerHelper('empty', arr => arr.length === 0);
-	Handlebars.registerHelper('isChildOf', (sourceId, parentId) => {
-		let result = false;
-		const library = game.settings.get('syrinscape', 'sound-library');
-
-		let parentList = [];
-		const searchChildren = (children: Array<any>) => {
-			for (let childIdx = 0; childIdx < children.length; childIdx++) {
-				const child = children[childIdx];
-
-				if (child.id === sourceId) {
-					result = parentList.includes(parentId);
-					return true;
-				} else if (child.type === 'folder') {
-					parentList.push(child.id);
-
-					if (searchChildren(child.children)) {
-						return true;
-					}
-
-					parentList.pop();
-				}
-			}
-
-			return false;
-		};
-
-		for (let i = 0; i < library.length; i++) {
-			const obj = library[i];
-
-			if (obj.id === sourceId) {
-				break;
-			} else if (obj.type === 'folder') {
-				parentList.push(obj.id);
-
-				if (searchChildren(obj.children)) {
-					break;
-				}
-
-				parentList.pop();
-			}
-		}
-
-		return result;
-	});
+	registerHandlebars();
 
 	// Preload Handlebars templates
 	await preloadTemplates();
